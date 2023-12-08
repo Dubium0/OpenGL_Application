5b268ed79b994df409f0fd846d7233b13b9e4f53
Yunus Emre ASLAN <40874281+Dubium0@users.noreply.github.com>
2023-12-08 13:37:39 +0300

    Add project files.

---
+++ OpenGL_Application.cpp
@@ -0,0 +1,12 @@
+﻿// OpenGL_Application.cpp : Defines the entry point for the application.
+//
+
+#include "OpenGL_Application.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
